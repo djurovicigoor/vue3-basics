6dc84952cc2b1b97cefeeb239e69ff507f6bc553
Djurović Igor <djurovic.igoor@gmail.com>
2023-08-02 14:11:11 +0200

    composirion api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+let message = ref('Hello World')
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>
+      {{ message }}
+    </p>
+    <p>
+      <input type="text" v-model="message" />
+    </p>
   </main>
 </template>
