af4bde35414616f86334958566f10d87dace9fd7
Djurović Igor <djurovic.igoor@gmail.com>
2023-08-04 22:15:17 +0200

    modal and transitions

---
+++ src/components/Modal.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps({
+  show: Boolean
+})
+</script>
+
+<template>
+  <Transition
+          enter-from-class="opacity-0"
+          enter-to-class="opacity-100"
+          enter-active-class="transition duration-300"
+          leave-active-class="transition duration-150"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+  >
+    <div
+      style="
+        position: fixed;
+        inset: 0;
+        background: rgba(0, 0, 0, 0.6);
+        display: grid;
+        place-items: center;
+      "
+      v-if="show"
+    >
+      <div style="background: white; padding: 1rem; width: 80vw; max-width: 500px">
+        <header>
+          <slot name="header"></slot>
+        </header>
+        <div>
+          <slot></slot>
+        </div>
+        <footer>
+          <slot name="footer">
+            <button @click="$emit('close')" class="">Close</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped></style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,18 +1,29 @@
 <script setup>
-import {useTeamStore} from "@/stores/teamStore.js";
+import { useTeamStore } from '@/stores/teamStore.js'
 import TeamHeader from '@/components/Teams/TheHeader.vue'
 import TeamMembers from '@/components/Teams/TeamMembers.vue'
 import TheFooter from '@/components/Teams/TheFooter.vue'
+import Modal from '@/components/Modal.vue'
+import { ref } from 'vue'
 
-let team = useTeamStore();
-team.fill();
+let team = useTeamStore()
+team.fill()
+
+let showModal = ref(false)
 </script>
 
 <template>
-  <TeamHeader/>
+  <TeamHeader @add="showModal = true"/>
   <div class="place-self-center flex flex-col gap-y-3">
     <TeamMembers />
   </div>
 
-  <TheFooter />
+  <TheFooter  />
+
+    <Modal :show="showModal" @close="showModal = false">
+        <form class="flex gap-2 mt-2">
+            <input type="email" placeholder="Email address" class="flex-1">
+            <button>Add</button>
+        </form>
+    </Modal>
 </template>
